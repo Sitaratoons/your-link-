835e4e3858781a289d11e850a237903b993035cd
Sitaratoons <mrajayh393798@gmail.com>
2025-02-06 16:26:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -29,6 +29,7 @@ from config import (
     OWNER_ID,
     START_PIC,
     FORCE_PIC,
+    AUTH_CHANNEL,
 )
 from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status, get_exp_time
 from database.database import add_user, del_user, full_userbase, present_user
